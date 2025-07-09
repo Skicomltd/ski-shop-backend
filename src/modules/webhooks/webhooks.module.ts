1943f1e1859e4cef25a89fe2b003ac2c9df95b59
taiwola <seunolanitori@gmail.com>
2025-07-09 12:16:44 +0100

    feat: add OrdersModule import to WebhooksModule for enhanced order handling

--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common"
 import { WebhookService } from "./webhook.service"
+import { OrdersModule } from "../orders/orders.module"
 
 @Module({
+  imports: [OrdersModule],
   providers: [WebhookService]
 })
 export class WebhooksModule {}
