8951f7846b6f98197b496842accd062673d10b80
Intuneteq <tobiolanitori@gmail.com>
2025-07-27 08:23:16 +0100

    fix: relsolved cart clearing after purchase is complete (#5)

--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -3,9 +3,10 @@ import { WebhookService } from "./webhook.service"
 import { OrdersModule } from "../orders/orders.module"
 import { WebhookController } from "./webhook.controller"
 import { PaymentsService } from "../services/payments/payments.service"
+import { CartsModule } from "../carts/carts.module"
 
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, CartsModule],
   controllers: [WebhookController],
   providers: [WebhookService, PaymentsService]
 })
