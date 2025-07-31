9a2dbe8c61666164caedc290a7c5cd6a4ea377f5
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2025-07-31 14:20:06 +0000

    Fixes coding style

--- src/modules/withdrawals/withdrawals.controller.spec.ts
+++ src/modules/withdrawals/withdrawals.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { WithdrawalsController } from './withdrawals.controller';
-import { WithdrawalsService } from './withdrawals.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { WithdrawalsController } from "./withdrawals.controller"
+import { WithdrawalsService } from "./withdrawals.service"
 
-describe('WithdrawalsController', () => {
-  let controller: WithdrawalsController;
+describe("WithdrawalsController", () => {
+  let controller: WithdrawalsController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WithdrawalsController],
-      providers: [WithdrawalsService],
-    }).compile();
+      providers: [WithdrawalsService]
+    }).compile()
 
-    controller = module.get<WithdrawalsController>(WithdrawalsController);
-  });
+    controller = module.get<WithdrawalsController>(WithdrawalsController)
+  })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(controller).toBeDefined()
+  })
+})
