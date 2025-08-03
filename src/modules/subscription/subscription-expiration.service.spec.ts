81c1b60ae583fec81465eda3b140a6a1cf4882ca
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2025-08-03 19:49:14 +0000

    Fixes coding style

--- src/modules/subscription/subscription-expiration.service.spec.ts
+++ src/modules/subscription/subscription-expiration.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SubscriptionExpirationService } from './subscription-expiration.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { SubscriptionExpirationService } from "./subscription-expiration.service"
 
-describe('SubscriptionExpirationService', () => {
-  let service: SubscriptionExpirationService;
+describe("SubscriptionExpirationService", () => {
+  let service: SubscriptionExpirationService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubscriptionExpirationService],
-    }).compile();
+      providers: [SubscriptionExpirationService]
+    }).compile()
 
-    service = module.get<SubscriptionExpirationService>(SubscriptionExpirationService);
-  });
+    service = module.get<SubscriptionExpirationService>(SubscriptionExpirationService)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(service).toBeDefined()
+  })
+})
