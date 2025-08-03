e2161766d2844523195536a82501cfc40585076b
Intuneteq <tobiolanitori@gmail.com>
2025-08-03 14:55:22 +0100

    feat: added totalSales to orders metadata

--- src/modules/orders/interceptors/orders.interceptor.ts
+++ src/modules/orders/interceptors/orders.interceptor.ts
@@ -6,24 +6,36 @@ import { map, Observable } from "rxjs"
 import { PaginationParams } from "@/modules/services/pagination/interfaces/paginationParams.interface"
 import { IOrderResponse } from "../interfaces/order-response.interface"
 import { PaginationService } from "@/modules/services/pagination/pagination.service"
+import { Request } from "express"
+import { UserRoleEnum } from "@/modules/users/entity/user.entity"
+import { OrderItemService } from "../orderItem.service"
 
 type PayloadType = [Order[], number]
 
 @Injectable()
 export class OrdersInterceptor extends OrderResponseMapper implements NestInterceptor<PayloadType, IOrdersResponse> {
-  constructor(private paginationService: PaginationService) {
+  constructor(
+    private paginationService: PaginationService,
+    private readonly orderItemService: OrderItemService
+  ) {
     super()
   }
 
-  intercept(context: ExecutionContext, next: CallHandler<PayloadType>): Observable<IOrdersResponse> | Promise<Observable<IOrdersResponse>> {
-    const request = context.switchToHttp().getRequest()
+  async intercept(context: ExecutionContext, next: CallHandler<PayloadType>): Promise<Observable<IOrdersResponse>> {
+    const request = context.switchToHttp().getRequest<Request>()
     const { page, limit } = request.query
+    const user = request.user
+    const metadata: Record<string, any> = {}
 
-    return next.handle().pipe(map((data) => this.paginate(data, { page, limit })))
+    if (user.role === UserRoleEnum.Vendor) {
+      metadata.totalSales = await this.orderItemService.totalSales(user.id)
+    }
+
+    return next.handle().pipe(map((data) => this.paginate(data, { page: Number(page), limit: Number(limit) }, metadata)))
   }
 
-  paginate([orders, total]: PayloadType, params: PaginationParams): IOrdersResponse {
+  paginate([orders, total]: PayloadType, params: PaginationParams, metadata?: Record<string, any>): IOrdersResponse {
     const data = orders.map((order) => this.transform(order))
-    return this.paginationService.paginate<IOrderResponse>(data, total, params)
+    return this.paginationService.paginate<IOrderResponse>(data, total, params, metadata)
   }
 }
