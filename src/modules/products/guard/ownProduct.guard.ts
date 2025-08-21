8def1b2bf810966cf8e8d916501d7e772a9d9f32
Intuneteq <tobiolanitori@gmail.com>
2025-08-21 09:05:00 +0100

    chore: return 404 when product not found PATCH /products

--- src/modules/products/guard/ownProduct.guard.ts
+++ src/modules/products/guard/ownProduct.guard.ts
@@ -2,6 +2,7 @@ import { Request } from "express"
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
 
 import { ProductsService } from "../products.service"
+import { NotFoundException } from "@/exceptions/notfound.exception"
 
 @Injectable()
 export class OwnProductGuard implements CanActivate {
@@ -10,6 +11,7 @@ export class OwnProductGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>()
     const productData = await this.productsService.findById(request.params.id)
+    if (!productData) throw new NotFoundException("product not found")
     return !productData ? false : productData.user.id === request.user.id
   }
 }
