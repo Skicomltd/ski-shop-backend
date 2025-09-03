2104ce022b22482b70d0140639e32d4df3dfa931
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2025-09-03 17:55:30 +0000

    Fixes coding style

--- src/modules/contact-us/contact-us.service.spec.ts
+++ src/modules/contact-us/contact-us.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ContactUsService } from './contact-us.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { ContactUsService } from "./contact-us.service"
 
-describe('ContactUsService', () => {
-  let service: ContactUsService;
+describe("ContactUsService", () => {
+  let service: ContactUsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContactUsService],
-    }).compile();
+      providers: [ContactUsService]
+    }).compile()
 
-    service = module.get<ContactUsService>(ContactUsService);
-  });
+    service = module.get<ContactUsService>(ContactUsService)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(service).toBeDefined()
+  })
+})
