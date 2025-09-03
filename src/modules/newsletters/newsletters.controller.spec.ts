34644ec655188e0b1e9c01b3f2770fb64e3af296
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2025-09-03 17:48:02 +0000

    Fixes coding style

--- src/modules/newsletters/newsletters.controller.spec.ts
+++ src/modules/newsletters/newsletters.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NewslettersController } from './newsletters.controller';
-import { NewslettersService } from './newsletters.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { NewslettersController } from "./newsletters.controller"
+import { NewslettersService } from "./newsletters.service"
 
-describe('NewslettersController', () => {
-  let controller: NewslettersController;
+describe("NewslettersController", () => {
+  let controller: NewslettersController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NewslettersController],
-      providers: [NewslettersService],
-    }).compile();
+      providers: [NewslettersService]
+    }).compile()
 
-    controller = module.get<NewslettersController>(NewslettersController);
-  });
+    controller = module.get<NewslettersController>(NewslettersController)
+  })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(controller).toBeDefined()
+  })
+})
--- src/modules/newsletters/newsletters.service.spec.ts
+++ src/modules/newsletters/newsletters.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NewslettersService } from './newsletters.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { NewslettersService } from "./newsletters.service"
 
-describe('NewslettersService', () => {
-  let service: NewslettersService;
+describe("NewslettersService", () => {
+  let service: NewslettersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewslettersService],
-    }).compile();
+      providers: [NewslettersService]
+    }).compile()
 
-    service = module.get<NewslettersService>(NewslettersService);
-  });
+    service = module.get<NewslettersService>(NewslettersService)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(service).toBeDefined()
+  })
+})
