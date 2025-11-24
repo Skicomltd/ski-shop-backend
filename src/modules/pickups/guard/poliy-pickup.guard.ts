f1297d8119646ad672a84e34e8cb5ab0b6fa97ac
Intuneteq <tobiolanitori@gmail.com>
2025-11-24 21:53:42 +0000

    chore(cleanup): remove unsed variables

--- src/modules/pickups/guard/poliy-pickup.guard.ts
+++ src/modules/pickups/guard/poliy-pickup.guard.ts
@@ -3,7 +3,6 @@ import { PolicyHandler } from "@/modules/auth/interface/policies-interface"
 import { AppAbility, CaslAbilityFactory } from "@/services/casl/casl-ability.factory"
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
 import { Reflector } from "@nestjs/core"
-import { Observable } from "rxjs"
 
 @Injectable()
 export class PolicyPickupGuard implements CanActivate {
