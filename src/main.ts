db23f467800063c534cb41e444eda72abf1bd47f
Intuneteq <tobiolanitori@gmail.com>
2025-12-29 13:56:27 +0000

    deploy: running app from env port

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { NestExpressApplication } from "@nestjs/platform-express"
 import helmet from "helmet"
 import * as compression from "compression"
 import { join } from "path"
+import { ConfigService } from "@nestjs/config"
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true })
+  const configService = app.get(ConfigService)
 
   app.enableCors({ origin: "*", credentials: true })
   app.setGlobalPrefix("api/v1", { exclude: [""] })
@@ -17,6 +19,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, "..", "views"))
   app.setViewEngine("hbs")
 
-  await app.listen(5000)
+  await app.listen(configService.get("app.port"))
 }
 bootstrap()
